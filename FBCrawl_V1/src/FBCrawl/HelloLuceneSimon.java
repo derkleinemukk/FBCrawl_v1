28f04a4bcb27d6e30ff9adc81b043acc84203bd2
derkleinemukk <simon.widler@web.de>
2015-03-13 11:58:51 +0100

    date searchable

--- FBCrawl_V1/src/FBCrawl/HelloLuceneSimon.java
+++ FBCrawl_V1/src/FBCrawl/HelloLuceneSimon.java
@@ -27,6 +27,7 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.SortField;
+import org.apache.lucene.search.SortField.Type;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.TopFieldDocs;
@@ -39,12 +40,11 @@ import org.apache.lucene.util.Version;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 public class HelloLuceneSimon implements Comparable {
 	String input = "";
 	IndexWriter w;
 	StandardAnalyzer analyzer = null;
-Directory index = null;
+	Directory index = null;
     
 
     IndexWriter indexWriter = null;
@@ -79,18 +79,23 @@ Directory index = null;
     	//add string 1
     	
     	System.out.println("id" + jsonObject.getString("id"));	
-    	System.out.println("name" + jsonObject.getString("name"));
+    	//System.out.println("name" + jsonObject.getString("name"));
     	//System.out.println("description" + jsonObject.getString("description"));
-    	System.out.println("attending_count" + jsonObject.getString("attending_count"));
-		System.out.println("declined_count" + jsonObject.getString("declined_count"));
-		System.out.println("invited_count" + jsonObject.getString("invited_count"));
+    	//System.out.println("attending_count" + jsonObject.getString("attending_count"));
+		//System.out.println("declined_count" + jsonObject.getString("declined_count"));
+		//System.out.println("invited_count" + jsonObject.getString("invited_count"));
 		System.out.println("start_time" + jsonObject.getString("start_time"));
 	
 		
 	doc.add(new TextField("description", jsonObject.getString("description"), Field.Store.YES));
 		doc.add(new TextField("id", jsonObject.getString("id"), Field.Store.YES));
 		doc.add(new TextField("name", jsonObject.getString("name"), Field.Store.YES));
-		doc.add(new TextField("start_time", jsonObject.getString("start_time"), Field.Store.YES));
+		String startTime = jsonObject.getString("start_time");
+		String strOut = startTime.substring(0,10);
+		System.out.println(strOut);
+		String date = strOut.replace("-", "");
+		
+		doc.add(new TextField("start_time", date, Field.Store.YES));
 		   doc.add(new IntField("attending_count", jsonObject.getInt("attending_count"), Field.Store.YES));
 		   doc.add(new IntField("declined_count", jsonObject.getInt("declined_count"), Field.Store.YES));
 		   doc.add(new IntField("invited_count", jsonObject.getInt("invited_count"), Field.Store.YES));
@@ -112,7 +117,7 @@ Directory index = null;
     	
     	Query q = null;
     	try {
-    		q = new QueryParser(Version.LUCENE_40, "description", analyzer).parse(querystr);
+    		q = new QueryParser(Version.LUCENE_40, "start_time", analyzer).parse(querystr);
     	} catch (org.apache.lucene.queryparser.classic.ParseException e) {
     		e.printStackTrace();
     	}
@@ -122,30 +127,59 @@ Directory index = null;
     	IndexReader reader = DirectoryReader.open(this.index);
     	IndexSearcher searcher = new IndexSearcher(reader);
     	TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+    	
     	searcher.search(q, collector);
-       	System.out.println(q);
+
+       	
+    	System.out.println(q);
     	ScoreDoc[] scoreDocs = collector.topDocs().scoreDocs;
     	System.out.println("Found " + scoreDocs.length + " hits.");
+    	
 		for (int i = 0; i < scoreDocs.length; ++i) {
 			int docId = scoreDocs[i].doc;
 			Document d = searcher.doc(docId);
 			System.out.println((i + 1) + ". " + d.get("id") + "\t" + d.get("name") + d.get("attending_count") + "\t" + d.get("attending_count") + "\t"+ d.get("declined_count") + "\t"+ d.get("invited_count") + "\t"+ d.get("start_time") + "\t");
 
 		}
+		
+		
+		
+		Sort sorter = new Sort(); // new sort object
+
+    	String field = "attending_count"; 
+    	Type type = Type.INT;
+    	boolean descending = true; 
+
+    	SortField sortField = new SortField(field, type, descending);
+    	sorter.setSort(sortField, SortField.FIELD_SCORE);
     	
+		
+		System.out.println("Sorted");    	
+		TopFieldDocs docs = searcher.search(q,20, sorter);
+		ScoreDoc[] hits= docs.scoreDocs;
+
+		for (int j=0; j < hits.length; j++) {
+			int docId = hits[j].doc;
+			Document d = searcher.doc(docId);
+			System.out.println((j+ 1) + ". " + d.get("id") + "\t" + d.get("name") + d.get("attending_count") + "\t" + d.get("attending_count") + "\t"+ d.get("declined_count") + "\t"+ d.get("invited_count") + "\t"+ d.get("start_time") + "\t");
+		}
+		System.out.println("yep");
+
+    	
+
     	
     }  
     
 
-//    public void sortedSearch(String[] input2)     	
-//    	throws IOException, ParseException{
-//    	 		
-//    	
-//    	IndexReader reader = DirectoryReader.open(this.index);
-//    	IndexSearcher searcher = new IndexSearcher(reader);
-//    	  Query q=new MatchAllDocsQuery();
-//    	
-//    	  SortField sortField= new SortField("attending_count",SortField.INT,true);
+    //public void sortedSearch(String[] input2)     	
+    	//throws IOException, ParseException{
+    	 		
+    	
+    //	IndexReader reader = DirectoryReader.open(this.index);
+    	//IndexSearcher searcher = new IndexSearcher(reader);
+    	  //Query q=new MatchAllDocsQuery();
+    	
+    	  //SortField sortField= new SortField("attending_count",SortField.INT,true);
 //    	  Sort sort=new Sort(sortField);
 //    	  TopFieldDocs docs = searcher.search(q,10,sort);
 //    	  ScoreDoc[] hits=docs.scoreDocs;
@@ -154,6 +188,19 @@ Directory index = null;
 //    	  }
 //
 //    }
+    
+    public void sortAttendance(){
+    	Sort sorter = new Sort(); // new sort object
+
+    	String field = "attending_count"; 
+    	Type type = Type.INT;
+    	boolean descending = true; 
+
+    	SortField sortField = new SortField(field, type, descending);
+    	sorter.setSort(sortField, SortField.FIELD_SCORE);
+    	
+    	
+    }
     public void close() throws IOException{
     	
     	w.close();
